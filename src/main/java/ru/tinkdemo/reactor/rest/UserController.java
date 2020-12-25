e3eea5a34248f426ef80bbedd385eef66da79a26
Alexander Permyakov <a.i.permyakov@tinkoff.ru>
2020-12-25 18:31:20 +0300

    Add login server page controller and refactoring

---
+++ src/main/java/ru/tinkdemo/reactor/rest/UserController.java
@@ -0,0 +1,44 @@
+package ru.tinkdemo.reactor.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import ru.tinkdemo.reactor.domain.User;
+import ru.tinkdemo.reactor.service.UserService;
+import ru.tinkdemo.reactor.utils.JwtUtil;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private static final ResponseEntity<Object> UNAUTHORIZED = ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public Mono<ResponseEntity> login(ServerWebExchange exchange) {
+
+        return exchange
+                .getFormData()
+                .flatMap(
+                        credentials -> userService.findByUsername(
+                                credentials.getFirst("username")
+                        )
+                                .cast(User.class)
+                                .map(userDetails -> passwordEncoder.matches(
+                                        credentials.getFirst("password"),
+                                        userDetails.getPassword()
+                                        ) ?
+                                                ResponseEntity.ok(JwtUtil.generateToken(userDetails)) :
+                                                UNAUTHORIZED
+                                )
+                                .defaultIfEmpty(UNAUTHORIZED)
+                );
+    }
+}
--- src/main/java/ru/tinkdemo/reactor/utils/JwtUtil.java
+++ src/main/java/ru/tinkdemo/reactor/utils/JwtUtil.java
@@ -2,17 +2,24 @@ package ru.tinkdemo.reactor.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.tinkdemo.reactor.domain.User;
+import ru.tinkdemo.reactor.domain.UserRole;
 
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
     private static String secret;
+    @Value("${jwt.expiration}")
+    private static String expirationDurable;
 
     public static String extractUsername(String token) {
 
@@ -35,6 +42,30 @@ public class JwtUtil {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public static String generateToken(User user) {
+
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put(
+                "role",
+                user.getRoles()
+                        .stream()
+                        .map(UserRole::getName)
+                        .collect(Collectors.toList())
+        );
+
+        long expirationSeconds = Long.parseLong(expirationDurable);
+        Date currentDate = new Date();
+        Date expirationDate = new Date(currentDate.getTime() + expirationSeconds * 1000);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(currentDate)
+                .setExpiration(expirationDate)
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
     }
 
 }
