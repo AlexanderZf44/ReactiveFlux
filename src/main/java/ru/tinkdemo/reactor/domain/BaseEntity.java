dda9b3bc52cf134be50f967e96459aa35caf33d1
Alexander Permyakov <a.i.permyakov@tinkoff.ru>
2020-12-30 14:02:41 +0300

    Add comment for entity id field

--- src/main/java/ru/tinkdemo/reactor/domain/BaseEntity.java
+++ src/main/java/ru/tinkdemo/reactor/domain/BaseEntity.java
@@ -8,6 +8,14 @@ import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 
+/**
+ * Важный момент, Spring r2jdbs использует стратегию IsNewStrategy при проверке внесения сущности в БД.
+ * При save в repository идет проверка:
+ * если id поле null, если объект, или 0, если примитив - происходит Insert в БД;
+ * в любом другом случае - выполняется Update.
+ * Если не держать этот момент в уме, можно получить следующую ошибку:
+ * Failed to update table [table name]. Row with Id [value] does not exist.
+ */
 @Data
 @SuperBuilder
 @NoArgsConstructor
